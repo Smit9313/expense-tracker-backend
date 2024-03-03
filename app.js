975412aebc3708c1a6daa44e3a651957836d1157
Jaygb <jaygabani684@gmail.com>
2024-03-03 12:22:36 +0530

    bug fix

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const totalUserData = require('./routes/totalUserData')
 
 const app = express();
 
-app.use(cors());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 
