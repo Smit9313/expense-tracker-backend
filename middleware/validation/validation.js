e89870204795b88e8db0a235eddcf143d0651872
Smit9313 <smitdudhat10@gmail.com>
2024-03-19 21:25:46 +0530

    validation changes

--- middleware/validation/validation.js
+++ middleware/validation/validation.js
@@ -1,39 +1,41 @@
 var Joi = require("joi");
 const createApiResponse = require('../../helper/createApiResponse')
 
-function overallSchema(schema,req,res,next)  {   
-  const {error} = schema.validate(req.body,{abortEarly: false})
-  if(error){
-    return res.json(createApiResponse(false,error, "Validation error",404))
-  }else{
+function overallSchema(schema, req, res, next) {
+  const { error } = schema.validate(req.body, { abortEarly: false })
+  if (error) {
+    return res.json(createApiResponse(false, error, "Validation error", 404))
+  } else {
     next()
   }
 }
 
 exports.createExpenseMiddleware = (req, res, next) => {
   const schema = Joi.object({
-    expenseCategoryId:Joi.string().required(),
-    expenseDetails:Joi.string().required(),
-    expenseAmount:Joi.number().required(),
-    expenseDate:Joi.date().required(),
+    expenseId: Joi.string().required(),
+    expenseCategoryId: Joi.string().required(),
+    expenseDetails: Joi.string().required(),
+    expenseAmount: Joi.number().required(),
+    expenseDate: Joi.date().required(),
   });
-  overallSchema(schema,req,res,next)
+  overallSchema(schema, req, res, next)
 };
 
-exports.createExpCategoryMiddleware = (req,res,next)=>{
+exports.createExpCategoryMiddleware = (req, res, next) => {
   const schema = Joi.object({
     name: Joi.string().required()
   })
-  overallSchema(schema,req,res,next)
+  overallSchema(schema, req, res, next)
 }
 
-exports.createIncomeMiddleware = (req,res,next) => {
+exports.createIncomeMiddleware = (req, res, next) => {
   const schema = Joi.object({
-      incomeCategoryId:Joi.string().required(),
-      incomeDetails:Joi.string().required(),
-      incomeAmount:Joi.number().required(),
-      incomeDate:Joi.date().required()
+    incomeId: Joi.string().required(),
+    incomeCategoryId: Joi.string().required(),
+    incomeDetails: Joi.string().required(),
+    incomeAmount: Joi.number().required(),
+    incomeDate: Joi.date().required()
   })
-  overallSchema(schema,req,res,next)
+  overallSchema(schema, req, res, next)
 }
 
